b36daebd5ab4b0a9adadf9790cab579fa532d856
EMAD2274 <160025473+EMAD2274@users.noreply.github.com>
2024-02-15 20:55:28 +0200

    Update sendEmail.js

--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -10,14 +10,14 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-    user: usef.emad.2274@gmail.com,
-    pass: wqmy bdzi gvkg jzos,
+    user: process.env.GMAIL_EMAIL,
+    pass: process.env.GMAIl_PASSWORD,
   },
 });
 
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: usef.emad.2274@gmail.com, // sender address
+    from: `Fred Foo 👻" <${process.env.GMAIL_EMAI}>`, // sender address
     to: options.email, // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
